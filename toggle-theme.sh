fe6a386fd3bc6a0cbe4c3ff88ab84962d41bd31a
Marco Seguri <seguri@users.noreply.github.com>
2024-03-25 23:24:03 +0100

    Update script to toggl dark theme

--- toggle-theme.sh
+++ toggle-theme.sh
@@ -1,6 +1,13 @@
-#!/opt/homebrew/bin/bash
+#!/usr/bin/env bash
 
-set -eu
+set -euo pipefail
 
-toggle-theme-stdout.sh "$(dark-mode status)" > ~/.config/alacritty/alacritty.toml
+LIGHT_THEME="$HOME/Documents/alacritty-themes/themes/Brewer.light.toml"
+DARK_THEME="$HOME/Documents/alacritty-themes/themes/Brewer.dark.toml"
+DST="$HOME/.config/alacritty/active_theme.toml"
 
+if [[ $(dark-mode status) == on ]]; then
+    cp "$DARK_THEME" "$DST"
+else
+    cp "$LIGHT_THEME" "$DST"
+fi
